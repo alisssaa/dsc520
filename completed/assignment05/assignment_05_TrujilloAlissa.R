8159a11bd145048685a2464d779f6ae9d5821c17
Ali <alissatrujillo23@gmail.com>
2022-04-30 12:42:11 -0400

    completed exercise 7

--- completed/assignment05/assignment_05_TrujilloAlissa.R
+++ completed/assignment05/assignment_05_TrujilloAlissa.R
@@ -8,13 +8,13 @@ setwd("/Users/alissa/Documents/Grad/DSC 520/dsc520")
 ## Load the `data/r4ds/heights.csv` to
 heights_df <- read.csv("data/r4ds/heights.csv")
 
-## Using `cor()` compute correclation coefficients for
+## Using `cor()` compute correlation coefficients for
 ## height vs. earn
-___
+cor(heights_df$height, heights_df$earn)
 ### age vs. earn
-___
+cor(heights_df$age, heights_df$earn)
 ### ed vs. earn
-___
+cor(heights_df$ed, heights_df$earn)
 
 ## Spurious correlation
 ## The following is data on US spending on science, space, and technology in millions of today's dollars
@@ -22,4 +22,4 @@ ___
 ## Compute the correlation between these variables
 tech_spending <- c(18079, 18594, 19753, 20734, 20831, 23029, 23597, 23584, 25525, 27731, 29449)
 suicides <- c(5427, 5688, 6198, 6462, 6635, 7336, 7248, 7491, 8161, 8578, 9000)
-___
+cor(tech_spending, suicides)
